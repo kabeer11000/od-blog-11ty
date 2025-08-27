0b65651ea5d8c7382e49d87061a1418f8570716a
Kabeer Jaffri <kabeer11000@gmail.com>
2025-08-28 00:20:45 +0500

    Initial Commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -76,7 +76,7 @@ export default async function(eleventyConfig) {
 		},
 		metadata: {
 			language: "en",
-			title: "Blog Title",
+			title: "Other Dev Research",
 			subtitle: "This is a longer description about your blog.",
 			base: "https://example.com/",
 			author: {
